65e59103f382a49fbadac8f7d59a4fd6874e763b
ettoremessina <et.messina@gmail.com>
2019-11-24 16:14:07 +0100

    added CascadeHadamardGates qsharp code

---
+++ CascadeHadamardGates/qsharp/CascadeHadamardGates.qs
@@ -0,0 +1,31 @@
+// Etore Messina (c). All rights reserved.
+// Licensed under the MIT License.
+
+namespace ComputationalMindset.QuantumExperiments
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Measurement;
+
+    operation CascadeAdamardGates() : (Result, Result, Result, Result, Result)
+    {
+        using (q = Qubit[5])
+        {
+            H(q[1]);
+            H(q[2]);
+            H(q[3]);
+            H(q[4]);
+            H(q[2]);
+            H(q[3]);
+            H(q[4]);
+            H(q[3]);
+            H(q[4]);
+            H(q[4]);
+
+            let result = (M(q[0]), M(q[1]), M(q[2]), M(q[3]), M(q[4]));
+
+            ResetAll(q);
+            return result;
+        }
+    }
+}
---
+++ CascadeHadamardGates/qsharp/Driver.cs
@@ -0,0 +1,40 @@
+﻿// Ettore Messina (c). All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Simulation.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace ComputationalMindset.QuantumExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int shots = 1024;
+            int [] counters = new int[1 << 5];
+
+            var qDevice = new QuantumSimulator(throwOnReleasingQubitsNotInZeroState: true);
+            for (int i = 0; i < shots; ++i)
+            {
+                var (m0, m1, m2, m3, m4) = CascadeAdamardGates.Run(qDevice).Result;
+                int rnd =
+                      ((m0 == Result.One ? 1 : 0) << 0)
+                    + ((m1 == Result.One ? 1 : 0) << 1)
+                    + ((m2 == Result.One ? 1 : 0) << 2)
+                    + ((m3 == Result.One ? 1 : 0) << 3)
+                    + ((m4 == Result.One ? 1 : 0) << 4);
+                counters[rnd]++;
+            }
+
+            Console.WriteLine($"Bits\tValue\tCounter\tPerc");
+            for(int i = 0; i < counters.Length; ++i)
+            {
+                string binary = Convert.ToString(i, 2).PadLeft(5, '0');
+                double perc = Math.Round((counters[i] * 100.0) / shots, 2);
+                Console.WriteLine($"{binary}\t{i}\t{counters[i]}\t{perc}%");
+            }
+        }
+    }
+}
